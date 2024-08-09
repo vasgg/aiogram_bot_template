885f341147a0acf3cbb6f38c99dd2f0b73aea460
Vasily Gavrilov <102848740+vasgg@users.noreply.github.com>
2024-08-10 00:49:24 +0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -45,12 +45,12 @@ def get_logging_config(app_name: str):
         "disable_existing_loggers": False,
         "formatters": {
             "main": {
-                "format": "%(asctime)s.%(msecs)03d|%(levelname)s|%(module)s|%(funcName)s: %(message)s",
-                "datefmt": "%d.%m.%Y|%H:%M:%S%z",
+                "format": "%(asctime)s.%(msecs)03d [%(levelname)8s] [%(module)s:%(funcName)s:%(lineno)d] %(message)s",
+                "datefmt": "%d.%m.%Y %H:%M:%S%z",
             },
             "errors": {
-                "format": "%(asctime)s.%(msecs)03d|%(levelname)s|%(module)s|%(funcName)s: %(message)s",
-                "datefmt": "%d.%m.%Y|%H:%M:%S%z",
+                "format": "%(asctime)s.%(msecs)03d [%(levelname)8s] [%(module)s:%(funcName)s:%(lineno)d] %(message)s",
+                "datefmt": "%d.%m.%Y %H:%M:%S%z",
             },
         },
         "handlers": {
@@ -70,9 +70,10 @@ def get_logging_config(app_name: str):
                 "()": RotatingFileHandler,
                 "level": "INFO",
                 "formatter": "main",
-                "filename": f"logs/{app_name}_log.log",
+                "filename": f"logs/{app_name}.log",
                 "maxBytes": 5000000,
                 "backupCount": 3,
+                "encoding": "utf-8",
             },
         },
         "loggers": {
